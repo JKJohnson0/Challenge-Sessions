ac108b8b5ebb5c2546a273182750446c6fd8c395
JKJohnson0 <104327354+JKJohnson0@users.noreply.github.com>
2022-08-28 20:14:19 -0500

    Update twoSum.java
    changed twoSum

--- src/ChallengeList/twoSum.java
+++ src/ChallengeList/twoSum.java
@@ -2,9 +2,12 @@ package ChallengeList;
 
 public class twoSum {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+	public int[] twoSum(int[] nums, int target) {
+    for(int i = 0; i < nums.length; i++){
+        if(nums[i] + nums[i + 1] == target){
+            //return 
+            }    
+        }    
+    }
 
 }
